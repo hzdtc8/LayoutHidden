034fb8049dcef0abf8045c2293b386c408150263
Zheng Huang <zheng.huang@ndsu.edu>
2015-06-15 15:19:57 -0500

    change to Microsoft

---
+++ LayoutHidden/src/com/example/layouthidden/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.layouthidden;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+	LinearLayout mAinLayout;
+
+	Button visibleButton;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mAinLayout = (LinearLayout)findViewById(R.id.HelloLayout);
+
+
+		visibleButton = (Button)findViewById(R.id.button1);
+		
+		visibleButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				TextView gTextView=new TextView(MainActivity.this);
+				gTextView.setText("Hello,Microsoft");
+				mAinLayout.addView(gTextView);
+				
+			}
+		});
+		
+		
+
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+
+}
